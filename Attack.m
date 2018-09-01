1f6de3f8432957590704f5c326504b15acac93da
yuzuruhanyu <wjr19950102@163.com>
2018-09-01 21:06:29 +0800

    code for generating distorted copies

--- Attack.m
+++ Attack.m
@@ -4,8 +4,8 @@ clear
 
 filenum=3000;
 
-src_folder='D:\dataset\test\1Kbase';
-root_folder='D:\dataset\test\DEEP-DIST';
+src_folder='original';   % 3000 original images 
+root_folder='distorted';    % distorted images
 
 tot=0;
 
@@ -14,31 +14,31 @@ for method=0:8
     switch method
         case 0 % JPEG
             stren=[1,5,10:20:90,95];
-            dist='JPG';
-        case 1 % noise variance
+            dist='JPEG';
+        case 1 % Gaussian Noise
             stren=[0.01:0.01:0.05 0.1 0.15  0.2 0.25]; %NOISE2
-            dist='NOISE2';
-        case 2 % rotation
+            dist='Gaussian_Noise';
+        case 2 % rotation+cropping
             stren=[1:4,6:2:10];
-            dist='ROT';
-        case 3 % filter
+            dist='rotation+cropping';
+        case 3 % Median Filtering
             stren=2:2:20;
-            dist='MEDFLT';
-        case 4 % enhance
+            dist='Median_Filtering';
+        case 4 % Histogram Equalization
             stren=[8, 16, 32:32:224];
-            dist='ENH';
+            dist='Histogram_Equalization';
         case 5 % gamma correction
             stren=[0.55:0.1:0.95, 1.05:0.1:1.45];
             dist='GAMMA';
         case 6 % speckle noise
             stren=[0.01 0.05 0.1:0.1:0.3];
-            dist='SPCK';
-        case 7 % circular filtering
+            dist='Speckle_Noise';
+        case 7 % circular averaging filtering
            stren=[1 5 10:5:40];
-           dist='CIRFLT2';   
+           dist='CIRFLT';   
         case 8 % scaling
            stren=[0.2 0.4 0.5 2 4];
-           dist='SCALE1';
+           dist='SCALE';
     end
     
     dis_img_num=size(stren,2);
@@ -65,28 +65,28 @@ for method=0:8
                     imwrite(J,dest_path,'bmp');
                 end
                 
-            case 1 % noise variance
+            case 1 % Gaussian Noise
                 for k=1:dis_img_num
                     J = imnoise(I,'gaussian', 0,stren(k));
                     dest_path=fullfile(dist_folder, [num2str(k),'.bmp']);
                     imwrite(J,dest_path,'bmp');
                 end
                 
-            case 2 % rotation
+            case 2 % rotation+cropping
                 for k=1:dis_img_num
                     J = imrotate(I, stren(k), 'bilinear','crop');
                     dest_path=fullfile(dist_folder, [num2str(k),'.bmp']);
                     imwrite(J,dest_path,'bmp');
                 end
                 
-            case 3 % filter
+            case 3 % Median Filtering
                 for k=1:dis_img_num
                     J = medfilt2(I, [stren(k) stren(k)]);
                     dest_path=fullfile(dist_folder, [num2str(k),'.bmp']);
                     imwrite(J,dest_path,'bmp');
                 end
                 
-            case 4 % enhance
+            case 4 % Histogram Equalization
                 for k=1:dis_img_num
                     J = histeq(I, stren(k));
                     dest_path=fullfile(dist_folder, [num2str(k),'.bmp']);
@@ -107,7 +107,7 @@ for method=0:8
                     imwrite(J,dest_path,'bmp');
                 end
                 
-             case 7 % circular filtering
+             case 7 % circular averaging filtering
                 for k=1:dis_img_num
                     J=imfilter(I,fspecial('disk',stren(k)));
                     dest_path=fullfile(dist_folder, [num2str(k),'.bmp']);
